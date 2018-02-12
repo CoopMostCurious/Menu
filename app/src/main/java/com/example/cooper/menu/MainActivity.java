5f3590302598bc9356d89bfc7d86b7010bcf135d
Cooper Hanke <hanke31314@gmail.com>
2018-02-11 21:43:24 -0600

    possible solution to the exercise

--- app/src/main/java/com/example/cooper/menu/MainActivity.java
+++ app/src/main/java/com/example/cooper/menu/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.cooper.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
+        TextView text1 = findViewById(R.id.menu_item_1);
+        Log.i("Menu App","We have the delicious: " + text1);
 
         // Find second menu item TextView and print the text to the logs
+        TextView text2 = findViewById(R.id.menu_item_2);
+        Log.i("Menu App", "We have the delicious: " + text2);
 
         // Find third menu item TextView and print the text to the logs
+        TextView text3 = findViewById(R.id.menu_item_3);
+        Log.i("Menu App", "We have the delicious: " + text3);
 
     }
 }
